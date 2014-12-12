ec00e70f75fef3c0303ea691752aa3df7ac3b54c
Alexander Favorov <favorov@sensi.org>
2014-12-12 04:52:36 -0500

    trying to get better doc

--- backwards.rank.R
+++ backwards.rank.R
@@ -20,3 +20,26 @@ sort.by.backwards.rank<-function(correlations){
 	
 	t(sort.by.backwards.rank)	
 }
+
+
+#this function returns the rank of rev-rank of our gene (row)
+#in the tester gene's list (column)
+rank.backwards.rank<-function(correlations){
+	backwards.rank<-apply(-correlations,2, rank)
+
+	# negation is to have decreasing rank
+	
+	#we apply it by columns because our gene of interest is in row
+	#each column correspods to 'tester gene' and the rank 'our' genes by 
+	#the correlation with tester.
+
+	rank.backwards.rank<-apply(backwards.rank,1,rank)
+	# now, we rank tester by rank of our in the tester's list
+	# remember that our genes rows, testers are columns
+	rownames(rank.backwards.rank)<-rownames(correlations)
+	colnames(rank.backwards.rank)<-colnames(correlations)
+	#after t, our genes is row 
+	t(rank.backwards.rank)	
+}
+
+
