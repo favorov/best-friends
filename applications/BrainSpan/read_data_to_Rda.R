f3b970809d388540db8c49ef46457811080e4171
Alexander Favorov <favorov@sensi.org>
2015-03-31 17:02:25 -0400

    diagnostics

--- applications/BrainSpan/read_data_to_Rda.R
+++ applications/BrainSpan/read_data_to_Rda.R
@@ -5,12 +5,16 @@ genenames<-genenames<-read.table(log.file.name,stringsAsFactors = FALSE,colClass
 #genenames<-genenames[1:1000,]
 number_of_genes<-dim(genenames)[1]
 correlations <- data.frame(t(rep(0.,number_of_genes)))
+message('frame is created')
 correlations[number_of_genes,number_of_genes]=1.
+message('corner is set')
 colnames(correlations)=genenames[,2]
 rownames(correlations)=genenames[,2]
+message('names are set')
 for (ind in 1:number_of_genes-1) {
 	correlations[ind,ind]=1.
 }
+message('diagonal is set')
 nr.tsv <- file(corr.snake.name,"r")
 while(length(line <- readLines(nr.tsv, 1)) > 0) {
 	spli<-strsplit(line,split="\t")[[1]]
