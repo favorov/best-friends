53e17b416c1de9f179d1e99d9b3c5eb1773cb2df
Alexander Favorov <favorov@sensi.org>
2015-04-04 04:47:21 -0400

    going on with data.table

--- applications/BrainSpan/read_data_to_Rda.R
+++ applications/BrainSpan/read_data_to_Rda.R
@@ -5,6 +5,21 @@ gene.index<-fread(log.file.name)
 setnames(gene.index,1:3,c('idx','gene.id','cnt'))
 gene.idx<-gene.index[,idx]
 #debug
-correlations <- data.table(idx=gene.idx) 
+correlations <- data.table(idx=gene.idx)
+setkey(correlations,idx)
 message('frame is created')
+corr.data<-fread(corr.snake.name)
+setnames(corr.data,1:3,c('idx1','idx2','corr'))
+#curr.idx1 = -1
+#band.start = 1
+#band.end = 0 #inclusive
+#maxindex<-dim(corr.data[1])
+maxindex=1000000
+for (i in 1:maxindex)
+{
+	ind1<-corr.data[i,idx1]
+	ind2<-corr.data[i,idx2]
+	c<-corr.data[i,corr]
+	correlations[idx==ind1,ind2:=corr]
+}
 save(file='prenatal.correlations.matrix.Rda',list=c('correlations','gene.index'))
