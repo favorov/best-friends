24158866dddad16c9814f763886fed0921a9ad45
Alexander Favorov <favorov@sensi.org>
2015-04-21 15:01:35 -0400

    flie-reading version, started to work

--- applications/BrainSpan/read_prenatal_data_to_Rda.R
+++ applications/BrainSpan/read_prenatal_data_to_Rda.R
@@ -3,16 +3,20 @@ source('create_template_correlation_table_start.R')
 snake<-file(description = 'corr.min1.prenatal.nr.tsv',open='r')
 report.every<-1000000
 i<-0
-while(length(line<-scan(file=snake,nlines=1,what='character'))==3)
+line<-character(0)
+repeat
 {
-	i<i+1
+	line<-scan(file=snake,nlines=1,what='character',quiet=TRUE)
+	if (length(line)!=3) break
+	i<-i+1
 	if (i %% report.every == 0)
 		cat(paste0(i,"\n"))
-	ind1<-scan[1]
-	ind2<-scan[2]
-	c<-as,numeric(scan[3])
+	ind1<-line[1]
+	ind2<-line[2]
+	c<-as.numeric(line[3])
 	correlations[idx==ind1,eval(as.name(ind2)):=c]
 	correlations[idx==ind2,eval(as.name(ind1)):=c]
 }
+close(snake)
 save(file='prenatal.brainspan.corr.table.Rda',list=c('correlations','gene.index'))
 
