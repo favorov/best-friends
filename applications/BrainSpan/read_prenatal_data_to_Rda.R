61d7bafd60d5b6e25b7717b90b6f1f83e448b125
Alexander Favorov <favorov@sensi.org>
2015-04-23 20:57:11 -0400

    we will work with frame

--- applications/BrainSpan/read_prenatal_data_to_Rda.R
+++ applications/BrainSpan/read_prenatal_data_to_Rda.R
@@ -1,22 +1,39 @@
-source('create_template_correlation_table_start.R')
+source('create_template_identity_frame.R')
 #	save(file='template.brainspan.corr.table.Rda',list=c('correlations','gene.index')
 snake<-file(description = 'corr.min1.prenatal.nr.tsv',open='r')
-report.every<-1000000
-i<-0
-line<-character(0)
+message('Snake opened')
+block.size<-1000000
+gene.idx<-gene.index[,idx]
+max.gene.idx<-max(gene.idx)
+#we know that the idx's are 0-based
+rev.gen.idx<-integer(max.gene.idx+1)
+for(i in 1:length(gene.idx))
+	rev.gen.idx[gene.idx[i]+1]<-i
+#now to find index no by idx we say rev.gen.idx[idx+1]
+no<-0
+#no locals
+lines<-data.frame()
+id1<-id2<-''
+c<-0.0
+ind1<-ind2<-0
 repeat
 {
-	line<-scan(file=snake,nlines=1,what='character',quiet=TRUE)
-	if (length(line)!=3) break
-	i<-i+1
-	if (i %% report.every == 0)
-		cat(paste0(i,"\n"))
-	ind1<-line[1]
-	ind2<-line[2]
-	c<-as.numeric(line[3])
-	correlations[idx==ind1,eval(as.name(ind2)):=c]
-	correlations[idx==ind2,eval(as.name(ind1)):=c]
+	lines<-scan(file=snake,nlines=block.size,what=list('','',''),quiet=TRUE)
+	read.no<-length(lines[[1]])
+	if (read.no==0) break
+	no<-no+1
+	message(paste0('Block no ',no))
+	for (i in 1:read.no)
+	{
+		id1<-lines[[1]][i]
+		id2<-lines[[2]][i]
+		ind1<-rev.gen.idx[as.numeric(id1)+1]
+		ind2<-rev.gen.idx[as.numeric(id2)+1]
+		c<-as.numeric(lines[[3]][i])
+		correlations[id1,id2]=c
+		correlations[id2,id1]=c
+	}
 }
 close(snake)
-save(file='prenatal.brainspan.corr.table.Rda',list=c('correlations','gene.index'))
+save(file='prenatal.brainspan.corr.Rda',list=c('correlations','gene.index'))
 
