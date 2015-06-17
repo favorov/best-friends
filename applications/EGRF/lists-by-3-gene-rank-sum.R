3f65cbfcc44a7f4e644bc1911cb75aa39a28c128
Alexander Favorov <favorov@sensi.org>
2015-06-17 02:53:19 -0400

    NAs instead of 1s

--- applications/EGRF/lists-by-3-gene-rank-sum.R
+++ applications/EGRF/lists-by-3-gene-rank-sum.R
@@ -17,7 +17,7 @@ backwards.ranks<-apply(-1*HaCaT.Corr.Spearman.Diff,2, rank)
 order.to.three<-sapply(gene.names,function(g) backwards.ranks[g,'EGFR']+backwards.ranks[g,'ERBB2']+backwards.ranks[g,'ERBB3'])
 load('TRANSFAC_Genes_2014.Rda')
 gene.names<-setdiff(gene.names,c('EGFR','ERBB2','ERBB3'))
-TRASFAC.p.values<-sapply(TF2Gene,function(TF.list){
+TRANSFAC.p.values<-sapply(TF2Gene,function(TF.list){
 		in.list=intersect(TF.list,gene.names)
 		out.list=setdiff(gene.names,in.list)
 		if(length(in.list)==0 || length(out.list)==0) return(NA)
