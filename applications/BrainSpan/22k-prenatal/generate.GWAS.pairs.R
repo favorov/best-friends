11f112071692351feab27bdc762291e1669ff7a2
Alexander Favorov <favorov@sensi.org>
2015-06-17 02:51:05 -0400

    commets are better

--- applications/BrainSpan/22k-prenatal/generate.GWAS.pairs.R
+++ applications/BrainSpan/22k-prenatal/generate.GWAS.pairs.R
@@ -5,34 +5,77 @@ load(file = 'prenatal.22k.order.filtered.Rda')
 library('data.table')
 gwas.genes<-fread('lists/gwas-genes.txt',header = FALSE)[,V1]
 gwas.genes<-gwas.genes[gwas.genes %in% gene.symbols]
+list.1.length<-as.integer(length(gene.ensemble.ids)/100)
 list.5.length<-as.integer(length(gene.ensemble.ids)/20)
 
-sink('GWAS.RBR.pairs.txt')
+sink('GWAS.RBR.5.pairs.txt')
 for (gwas.gene in gwas.genes) {
   gwas.gene.no<-which(gwas.gene==gene.symbols)
   friends<-gene.symbols[order.22k.prenatal[,gwas.gene.no][1:list.5.length]]
   gwas.genes.by.me<-setdiff(gwas.genes,c(gwas.gene))
   my.gwas.friends<-gwas.genes.by.me[gwas.genes.by.me %in% friends]
-  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep=';\n')
+  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep='; ')
+	cat('; ')
 }
+cat('\n')
 sink()
 
-sink('GWAS.RBR.filtered.pairs.txt')
+sink('GWAS.RBR.5.filtered.pairs.txt')
 for (gwas.gene in gwas.genes) {
   gwas.gene.no<-which(gwas.gene==gene.symbols)
   friends<-gene.symbols[order.22k.prenatal.filtered[,gwas.gene.no][1:list.5.length]]
   gwas.genes.by.me<-setdiff(gwas.genes,c(gwas.gene))
   my.gwas.friends<-gwas.genes.by.me[gwas.genes.by.me %in% friends]
-  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep=';\n')
+  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep='; ')
+	cat('; ')
 }
+cat('\n')
 sink()
 
-sink('GWAS.naive.pairs.txt')
+sink('GWAS.naive.5.pairs.txt')
 for (gwas.gene in gwas.genes) {
   gwas.gene.no<-which(gwas.gene==gene.symbols)
   friends<-gene.symbols[order(corr.22k.prenatal[gwas.gene.no,],decreasing=TRUE)[1:list.5.length]]
   gwas.genes.by.me<-setdiff(gwas.genes,c(gwas.gene))
   my.gwas.friends<-gwas.genes.by.me[gwas.genes.by.me %in% friends]
-  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep=';\n')
+  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep='; ')
+	cat('; ')
 }
+cat('\n')
+sink()
+
+sink('GWAS.RBR.1.pairs.txt')
+for (gwas.gene in gwas.genes) {
+  gwas.gene.no<-which(gwas.gene==gene.symbols)
+  friends<-gene.symbols[order.22k.prenatal[,gwas.gene.no][1:list.1.length]]
+  gwas.genes.by.me<-setdiff(gwas.genes,c(gwas.gene))
+  my.gwas.friends<-gwas.genes.by.me[gwas.genes.by.me %in% friends]
+  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep='; ')
+	cat('; ')
+}
+cat('\n')
+sink()
+
+sink('GWAS.RBR.1.filtered.pairs.txt')
+for (gwas.gene in gwas.genes) {
+  gwas.gene.no<-which(gwas.gene==gene.symbols)
+  friends<-gene.symbols[order.22k.prenatal.filtered[,gwas.gene.no][1:list.1.length]]
+  gwas.genes.by.me<-setdiff(gwas.genes,c(gwas.gene))
+  my.gwas.friends<-gwas.genes.by.me[gwas.genes.by.me %in% friends]
+  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep='; ')
+	cat('; ')
+}
+cat('\n')
+sink()
+
+sink('GWAS.naive.1.pairs.txt')
+for (gwas.gene in gwas.genes) {
+  gwas.gene.no<-which(gwas.gene==gene.symbols)
+  friends<-gene.symbols[order(corr.22k.prenatal[gwas.gene.no,],decreasing=TRUE)[1:list.1.length]]
+  gwas.genes.by.me<-setdiff(gwas.genes,c(gwas.gene))
+  my.gwas.friends<-gwas.genes.by.me[gwas.genes.by.me %in% friends]
+  cat(paste(gwas.gene,my.gwas.friends,sep=' -> '),sep='; ')
+	cat('; ')
+}
+cat('\n')
 sink()
