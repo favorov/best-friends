6350c66fd7cc396d48e000c0c51931c224418a4f
Alexander Favorov <favorov@sensi.org>
2017-09-09 13:49:41 +0300

    documentation

---
+++ pkg/R/best.friends.of.r
@@ -0,0 +1,14 @@
+#' best.friends.of: A package that provide the best-friend ranking
+#'
+#' Suppose We have some similarity of interaction measure for pairs of instnces, e.q. genes and regulators.
+#' We want to know what genes are the specifially best friends for selected regulators and vice versa.
+#'
+#' Two functions: 
+#' 
+#' @section best.friends.of functions:
+#'	RankByBestFriendOf takes the matrix, provide the ranks for best-friend ranking 
+#'	OrderByBestFriendOf takes the matrix, provide the orders by best-friend ranking 
+#'
+#' @docType package
+#' @name best.friends.of 
+NULL
