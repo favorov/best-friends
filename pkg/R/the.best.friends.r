d1b42cb8ccf87dfb08a5358ce1bf4adc21aabb74
Alexander Favorov <favorov@sensi.org>
2020-09-18 01:46:17 -0400

    working code

--- pkg/R/the.best.friends.r
+++ pkg/R/the.best.friends.r
@@ -34,50 +34,61 @@ best.friend.of<-function(relation,distance_like=FALSE){
 	data.frame(friend=as.integer(res[,1]),p.value=res[,2])
 }
 
-friend_and_p_value<-function(x) { #x is anumeric vector
-	#we are to find the difference of the best and the next; 
-	#we know that all values are between 0 and 1
-	#best and next are the smallest and the next
-	bestv <- 1.1;
-  prevv <- 1.1;
-	n<-length(x);
-	bestind<-n+1;
-	if (n<2) return(c(1,0));
-	for(i in c(1:n)) {
-		if (x[i]<bestv) {
-			prevv<-bestv;
-			bestv<-x[i];
-			bestind<-i;
-			next;
-		};
-		#if we are here, x[i] >= prevv
-		if (x[i]<prevv) {
-			prevv=x[i];
-		};
-	}
-	return (c(bestind,(1-prevv+bestv)^n));
+if(0) { #r code
+  friend_and_p_value<-function(x) { #x is anumeric vector
+  	#we are to find the difference of the best and the next; 
+  	#we know that all values are between 0 and 1
+  	#best and next are the smallest and the next
+  	bestv <- 1.1;
+    prevv <- 1.1;
+  	n<-length(x);
+  	bestind<-n+1;
+  	if (n<2) return(c(1,0));
+  	for(i in c(1:n)) {
+  		if (x[i]<bestv) {
+  			prevv<-bestv;
+  			bestv<-x[i];
+  			bestind<-i;
+  			next;
+  		};
+  		#if we are here, x[i] >= prevv
+  		if (x[i]<prevv) {
+  			prevv=x[i];
+  		};
+  	}
+  	return (c(bestind,(1-prevv+bestv)^n));
+  }
 }
 
 
-if(0) {
-cppFunction("
-	double bestminusnext(NumericVector x) {
-	//we are to find the difference of the best and the next; 
-	//we know that all values are between 0 and 1
-	//best and next are the smallest and the next
-	int n = x.size(), i; 
-	double best = 1.1, next=1.1;
-	if (n<2) return 0;
-	for(i = 0; i < n; i++) {
-		if (x[i]<best) {
-			next=best;
-			best=x[i];
-			continue;
-		};
+if(1) { #c++ code
+cppFunction('
+  #include <math.h>
+	NumericVector friend_and_p_value(NumericVector x) {
+	  //we are to find the difference of the best and the next; 
+	  //we know that all values are between 0 and 1
+	  //best and next are the smallest and the next
+	  int n = x.size(), i;
+	  double bestind=1.;
+	  double best = 1.1, next=1.1;
+	  if (n<2) {
+	    return (NumericVector::create(bestind,next-best,n));
+	  }
+	  for(i = 0; i < n; i++) {
+	  	if (x[i]<best) {
+		  	next=best;
+			  best=x[i];
+			  bestind=i+1;
+			  //return it as 1- based
+			  continue;
+		  }
 		//if we are here, x[i] >= best
-		if (x[i]<next) {
-			next=x[i];
-		};
-		return next-best;
-	}")
+		  if (x[i]<next) {
+			  next=x[i];
+		  }
+	  };
+		return (NumericVector::create(bestind,pow(1.-next+best,n)));
+	}'
+)
 }
+
