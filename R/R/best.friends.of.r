89849c5dff4b54c951b4540fec4d7a15a36fbb1c
Alexander Favorov <favorov@sensi.org>
2021-04-08 01:53:08 -0400

    trying to make index docs

--- R/R/best.friends.of.r
+++ R/R/best.friends.of.r
@@ -6,8 +6,9 @@
 #' Main function: 
 #' 
 #' @section friends.of functions:
-#' best.friends.of.features takes the features x ettities matrix, and for each feature provides the index of an entity that is the potential best friend of the featurefand the corresponding p-value. The p-value is about the null hypothesis that the difference between the best and the nest index is a result of randomess.  
-#' friends.of.features does the same, but it considers n (possibly, all) the features as potential friends, and p-values is generated for each feature+entity pair. The p-values tests the null hyposthesis that claims that the differnce of the feature's index in this entity and in the next-by-friendship entity is by random. 
+#' [best.friends.of.features] takes the features x ettities matrix, and for each feature provides the index of an entity that is the potential best friend of the featurefand the corresponding p-value. The p-value is about the null hypothesis that the difference between the best and the nest index is a result of randomess.
+#'
+#' [friends.of.features] does the same, but it considers n (possibly, all) the entities as potential friends for each ferature, and p-values is generated for each feature+entity pair. The p-values tests the null hyposthesis that claims that the differnce of the feature's index in this entity and in the next-by-friendship entity is by random. So, if the feature reliably separates the entity form the next one, the p-value is low.
 #'
 #' @docType package
 #' @name best.friends.of
