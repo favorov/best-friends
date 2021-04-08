9c4f71504d3ce98ff2da186477ffa2dfb4d70b0e
Alexander Favorov <favorov@sensi.org>
2021-04-08 02:04:18 -0400

    trying to make index docs

--- R/R/best.friends.of.r
+++ R/R/best.friends.of.r
@@ -4,10 +4,10 @@
 #' We want to know what genes are the specifically best friends for selected regulators and vice versa.
 #'
 #' @section best.friends.of functions:
-#' [best.friends.of.features] takes the features x ettities matrix, and for each feature provides the index of an entity that is the potential best friend of the featurefand the corresponding p-value. The p-value is about the null hypothesis that the difference between the best and the nest index is a result of randomess.
+#' [best.friends.of.features()] takes the features x ettities matrix, and for each feature provides the index of an entity that is the potential best friend of the featurefand the corresponding p-value. The p-value is about the null hypothesis that the difference between the best and the nest index is a result of randomess.
 #'
 #'
-#' [friends.of.features] does the same, but it considers n (possibly, all) the entities as potential friends for each ferature, and p-values is generated for each feature+entity pair. The p-values tests the null hyposthesis that claims that the differnce of the feature's index in this entity and in the next-by-friendship entity is by random. So, if the feature reliably separates the entity form the next one, the p-value is low.
+#' [friends.of.features()] does the same, but it considers n (possibly, all) the entities as potential friends for each ferature, and p-values is generated for each feature+entity pair. The p-values tests the null hyposthesis that claims that the differnce of the feature's index in this entity and in the next-by-friendship entity is by random. So, if the feature reliably separates the entity form the next one, the p-value is low.
 #'
 #' @docType package
 #' @name best.friends.of
