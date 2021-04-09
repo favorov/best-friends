bd9be63d0a2f0e2074c41574d0512dfdf966d018
Alexander Favorov <favorov@sensi.org>
2021-04-09 12:20:58 -0400

    Vignette text goes on

--- R/R/zzz.R
+++ R/R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
        version <- packageDescription("best.friends.of", field="Version")
-       packageStartupMessage(paste("Welcome to best.friands.of version", version,"stay tuned!"))
+       packageStartupMessage(paste("Welcome to best.friands.of version", version," and stay tuned!"))
 }
