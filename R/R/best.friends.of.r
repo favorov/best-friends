134a421e6ef01cde990b0dd4db4b298b03ae2eec
Alexander Favorov <favorov@sensi.org>
2021-08-06 10:57:07 +0300

    we are changing all the terminology and names

--- R/R/best.friends.of.r
+++ R/R/best.friends.of.r
@@ -1,4 +1,4 @@
-#' best.friends.of: A package that describe wheteher a community is a best friend (or one of the best friends) for its member. 
+#' best.friends: A package that describe wheteher a community is a best friend (or one of the best friends) for its member. 
 #'
 #' We have a list of intersecting communities, and the membership of all the elements is represened by real value. 
 #' It can be a fuzzy inclusion or, maybe, different status of members in a  communities. The absence of a relation (E is not a member of C at all)
@@ -10,15 +10,15 @@
 #' The member is a marker for the community (if we see him, all the community is around) and the community is friendly to him (all others do not like him).
 #' We say that the community is a friend (or best friend) of an element if it prefers the element more than other commities do and the difference is unlikely to be at random.
 #'
-#' @section best.friends.of functions:
-#' [best.friend.of.test] takes the elements x communities matrix, and for each element provides the community that is the potential best friend of the element and the corresponding p-value. The p-value is about the null hypothesis that the difference between the best and the next ranks of the element in communities a result of randomess. If p-value is low and the null is rejected, the community is the best friend of the element and the element is the community marker.
+#' @section best.friends functions:
+#' [best.friend.test] takes the elements x communities matrix, and for each element provides the community that is the potential best friend of the element and the corresponding p-value. The p-value is about the null hypothesis that the difference between the best and the next ranks of the element in communities a result of randomess. If p-value is low and the null is rejected, the community is the best friend of the element and the element is the community marker.
 #'
 #'
-#' [friends.of.test] does the same, but it considers n (possibly, all) the communities as potential friends for each member, and p-values are generated for each element+community pair. The p-values tests the null hyposthesis that claims that the differnce of the elements's ranks in this community and in the next-by-member-rank-of-the-element community is by random. I p-value is low and the null is rejected, the element reliably separates the comumities.
+#' [friends.test] does the same, but it considers n (possibly, all) the communities as potential friends for each member, and p-values are generated for each element+community pair. The p-values tests the null hyposthesis that claims that the differnce of the elements's ranks in this community and in the next-by-member-rank-of-the-element community is by random. If p-value is low and the null is rejected, the element reliably separates the comumities.
 #'
 #' @docType package
-#' @name best.friends.of
-#' @useDynLib best.friends.of
+#' @name best.friends
+#' @useDynLib best.friends
 #' @importFrom Rcpp evalCpp
 # these two are Rcpp - scecific ivocations 
 NULL
--- R/R/the.best.friends.r
+++ R/R/the.best.friends.r
@@ -1,17 +1,16 @@
-#best-friends-of library
-#A. Favorov, V. Ramensky, A. Mironov (c) 2014-2021
+#best.friends library
+#A. Favorov, V. Mukhina, V. Ramensky, A. Mironov (c) 2014-2021
 #'
 #'
-#' best.friends.of.features
+#' best.friends.test
 #'
-#' We have someones to be a friend (entity) as columns and we have what is to be friends of (features) as rows.
-#` matrix where in each column and row there is a value of how the raw specifically friendly to this column.
-#' @param relation is the fetures*entities matrix of the relations between features and the entities
-#' @param distance_like the default is \code{FALSE} and it shows that the relation values are not like distance, i.e. the better relation is shown by the lagrer value; is the relation is, on the contrary, distance-like, and 0 is the best, the value is \code{TRUE}.
-#' @return \code{data.frame} with 5 columns: feature index, friend entity index, uncorrected p-value for the pair, feature name, friend name 
-#' Best friend has the highest order, the worst has the lowest
+#' We have what can to be friends (communities) as columns and we have what they can befriends of (features) as rows.
+#` matrix where in each column and row there is a value of how strong the raw is related to this column.
+#' @param relation is the elements*communities matrix of the relations between features and the entities
+#' @param distance_like the default is \code{FALSE} and it shows that the relation values are not like distance, i.e. the better relation is shown by the lagrer value; if the relation is, on the contrary, distance-like, and 0 is the best, the value is \code{TRUE}.
+#' @return \code{data.frame} with 5 columns: element index, the index of the community that is a putative best friend of the element, uncorrected p-value for the pair, element name, friend name. The small (after correctio we are to do) p-value indicates that the community is really the best friend of the element 
 #' @export
-best.friends.of.features<-function(relation,distance_like=FALSE){
+best.friends.test<-function(relation,distance_like=FALSE){
   dims<-dim(relation)
   #if relation is distance_like, we will order in ascending
   #if nor, descending. 
@@ -20,7 +19,7 @@ best.friends.of.features<-function(relation,distance_like=FALSE){
   order<-ifelse(distance_like,1,-1)
   # if distance_like holds, the least is the best (first)
   #and order==1 (ascending) 
-  feature.ranks<-apply(relation,2, 
+  element.ranks<-apply(relation,2, 
           function(x){
             data.table::frankv(x,ties.method='average',
                                na.last=T,order=order)
@@ -29,27 +28,28 @@ best.friends.of.features<-function(relation,distance_like=FALSE){
   #if(!distance_like) {
   #  feature.ranks <- dims[1]+1 - feature.ranks
   #}
-	feature.ranks<-feature.ranks/dims[1]
+	element.ranks<-feature.ranks/dims[1]
 	#we applied ranking column-by-column (entity-by-entity); A's were ranked in each row,
-	res<-t(apply(feature.ranks,1,rank_diff_and_p_for_the_best))
+	res<-t(apply(element.ranks,1,rank_diff_and_p_for_the_best))
 	rn<-rownames(relation); if (length(rn)==0) {as.character(1:dim(relation)[1])} 
 	cn<-rownames(relation); if (length(cn)==0) {as.character(1:dim(relation)[2])} 
-	data.frame(feature=1:dims[1],friend=as.integer(res[,1]),p.value=res[,2],feature.name=rownames(relation),friend.name=colnames(relation)[as.integer(res[,1])])
+	data.frame(element=1:dims[1],friend=as.integer(res[,1]),p.value=res[,2],elemant.name=rownames(relation),friend.name=colnames(relation)[as.integer(res[,1])])
 }
 
 #'
-#' friends.of.features
+#' friends.test
 #'
-#' We have someones to be a friend (entity) as columns and we have what is to be friends of (features) as rows.
-#` matrix where in each column and row there is a value of how the raw specifically friendly to this column.
-#' @inheritParams best.friends.of.features
-#' @param friends_num number of entities we consider for each feature; the default -1 means all; if friends_num is 1, 
+#' We have what can to be friends (communities) as columns and we have what they can befriends of (features) as rows.
+#` matrix where in each column and row there is a value of how strong the raw is related to this column.
+#' @inheritParams best.friends.test
+#' @param friends.number number of entities we consider for each feature; the default -1 means all; if friends.number is 1, 
+#' the call do essentialy the same as the best.friends.test call
 #' @return \code{data.frame} with 4 columns: friend entity index, uncorrected p-value for the pair, feature name, friend name 
 #' Best friend has the highest order, the worst has the lowest
 #' @export
-friends.of.features<-function(relation,distance_like=FALSE,friends_num=-1){
+friends.of.features<-function(relation,distance_like=FALSE,friends.number=-1){
   dims<-dim(relation)
-	if(-1==friends_num){friends_num=dims[2]}
+	if(-1==friends_num){friends.number=dims[2]}
   order<-ifelse(distance_like,1,-1)
   #if relation is distance_like, we will order in ascending
   #if nor, descending. 
@@ -57,7 +57,7 @@ friends.of.features<-function(relation,distance_like=FALSE,friends_num=-1){
   #most close relations
   # if distance_like holds, the least is the best (first)
   #and order==1 (ascending) 
-  feature.ranks<-apply(relation,2, 
+  element.ranks<-apply(relation,2, 
                        function(x){
                          data.table::frankv(x,ties.method='average',
                                             na.last=T,order=order)
@@ -66,12 +66,12 @@ friends.of.features<-function(relation,distance_like=FALSE,friends_num=-1){
   #if(!distance_like) {
   #  feature.ranks <- dims[1]+1 - feature.ranks
   #}
-  feature.ranks<-feature.ranks/dims[1]
+  element.ranks<-element.ranks/dims[1]
   #we applied ranking column-by-column (entity-by-entity); A's were ranked in each row,
-  unlistres<-unlist(t(apply(feature.ranks,1,rank_diff_and_p_for_the_best_n,n=friends_num)))
+  unlistres<-unlist(t(apply(element.ranks,1,rank_diff_and_p_for_the_best_n,n=friends.number)))
   res<-list()
-	res$friends<-matrix(unlistres[seq(1,length(unlistres),2)],ncol = friends_num, nrow=dims[1], byrow = TRUE)
-	res$pvals<-matrix(unlistres[seq(2,length(unlistres),2)],ncol = friends_num, nrow=dims[1],byrow = TRUE)
+	res$friends<-matrix(unlistres[seq(1,length(unlistres),2)],ncol = friends.number, nrow=dims[1], byrow = TRUE)
+	res$pvals<-matrix(unlistres[seq(2,length(unlistres),2)],ncol = friends.number, nrow=dims[1],byrow = TRUE)
 	rownames(res$friends)<-rownames(relation)
 	rownames(res$pvals)<-rownames(relation)
 	res
