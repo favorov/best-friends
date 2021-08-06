4603ec9274f77c07959ab836b2805c4795266e0c
Alexander Favorov <favorov@sensi.org>
2021-08-06 12:10:46 +0300

    typo elemant fixed

--- R/R/the.best.friends.r
+++ R/R/the.best.friends.r
@@ -25,12 +25,12 @@ best.friends.test<-function(relation,distance_like=FALSE){
                                na.last=T,order=order)
           }
   )
-	element.ranks<-elemant.ranks/dims[1]
+	element.ranks<-element.ranks/dims[1]
 	#we applied ranking column-by-column (entity-by-entity); A's were ranked in each row,
 	res<-t(apply(element.ranks,1,rank_diff_and_p_for_the_best))
 	rn<-rownames(relation); if (length(rn)==0) {as.character(1:dim(relation)[1])} 
 	cn<-rownames(relation); if (length(cn)==0) {as.character(1:dim(relation)[2])} 
-	data.frame(element=1:dims[1],friend=as.integer(res[,1]),p.value=res[,2],elemant.name=rownames(relation),friend.name=colnames(relation)[as.integer(res[,1])])
+	data.frame(element=1:dims[1],friend=as.integer(res[,1]),p.value=res[,2],element.name=rownames(relation),friend.name=colnames(relation)[as.integer(res[,1])])
 }
 
 #'
