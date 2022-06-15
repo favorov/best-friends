737661c74ac44d9cb856ad18743a470d1557538d
Alexander Favorov <favorov@sensi.org>
2022-06-14 22:34:12 -0400

    attention!! (started to put the word in the docs)

--- R/best.friends/R/best.friends.r
+++ R/best.friends/R/best.friends.r
@@ -1,17 +1,18 @@
 #' best.friends: A package that describe wheteher a community is a best friend (or one of the best friends) for its member. 
 #'
-#' We have a list of intersecting communities, and the membership of all the elements is represened by real value. 
-#' It can be a fuzzy inclusion or, maybe, different status of members in a  communities. The absence of a relation (E is not a member of C at all)
-#' is supposed to be represented by the smallest value, naturally, it is 0 and all the memberships are positive. 
-#' The membership is a ExC matrix.
+#' We have a set of clouds and a set of tags and the relation of each tag and each cloud (attention that the cloud pays to the tag) is represented by a real value. 
+#' The absence of a relation is supposed to be represented by the smallest value, naturally, it is 0 and all the attentionvalues are are positive (not required). 
+#' The attention values is a TxC matrix.
 #' 
-#' We want to find the community that specifically prefers an element.
-#' We say that the community are  friendly to the elements. The simplest: suppose an element that is mebmer of only one community. 
-#' The member is a marker for the community (if we see him, all the community is around) and the community is friendly to him (all others do not like him).
-#' We say that the community is a friend (or best friend) of an element if it prefers the element more than other commities do and the difference is unlikely to be at random.
+#' We want to identify pairs of a tag and a cloud that specifically prefers the tag.
+#' We say that the cloud is friendly to the tag. The simplest: suppose that only one cloud pays attention to our tag. 
+#' The tag is a marker for the cloud (if we see the tag, the cloud is around) and the cloud is friendly to the tag (all others do not like him).
+#' We compare the degrees of friendship by ranking the ranks of attention -- first ranking is done inside the cloud - related column if the attention matrix -- and the second is inside the tag's row. 
+#' We say that the cloud is a friend (or best friend) of a tag if it more friendly to the tag more than other clouds do and the difference is unlikely to be at random.
 #'
 #' @section best.friends functions:
-#' [best.friends.test] takes the elements x communities matrix, and for each element provides the community that is the potential best friend of the element and the corresponding p-value. The p-value is about the null hypothesis that the difference between the best and the next ranks of the element in communities a result of randomess. If p-value is low and the null is rejected, the community is the best friend of the element and the element is the community marker.
+#' [best.friends.test] takes the tags x clouds attention matrix, and for each tag provides the cloud that is the potential best friend of the tag (actually, the most friendly cloud) and the corresponding p-value. 
+#' The p-value is the null hypothesis that for each cloud, the attention levels for all tags are i.i.d. values (the distrubution can be differennt for different tags). The statistics we use is the difference of friendness between the most friendly cloud and the next. If p-value is low and the null is rejected, the community is the best friend of the element and the element is the community marker.
 #'
 #'
 #' [friends.test] does the same, but it considers n (possibly, all) the communities as potential friends for each member, and p-values are generated for each element+community pair. The p-values tests the null hyposthesis that claims that the differnce of the elements's ranks in this community and in the next-by-member-rank-of-the-element community is by random. If p-value is low and the null is rejected, the element reliably separates the comumities.
