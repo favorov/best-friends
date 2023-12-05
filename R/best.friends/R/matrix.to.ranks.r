dc21be63778c8935c29c3739e1853b9d1949f3eb
Alexander Favorov <favorov@sensi.org>
2023-12-05 00:46:38 -0500

    starting to switch to step paragigm

--- R/best.friends/R/matrix.to.ranks.r
+++ R/best.friends/R/matrix.to.ranks.r
@@ -58,7 +58,7 @@ tag.unif.ks.test<-function(
         neglect_diagonal<-FALSE
       }
     }
-
+    
     order<-ifelse(distance_like,1,-1)
     #if attention is distance_like, we will order in ascending
     #if nor, descending. 
@@ -67,10 +67,10 @@ tag.unif.ks.test<-function(
     # if distance_like holds, the least is the best (first)
     #and order==1 (ascending) 
     ranks.of.tags<-apply(attention,2, 
-                     function(x){
-                       data.table::frankv(x,ties.method='average',
-                                          na.last=TRUE,order=order)
-                     }
+                         function(x){
+                           data.table::frankv(x,ties.method='average',
+                                              na.last=TRUE,order=order)
+                         }
     )
     #we applied ranking column-by-column (collection-by-cloud)
     rownames(ranks.of.tags)<-rownames(attention)
@@ -80,35 +80,4 @@ tag.unif.ks.test<-function(
   #it also signals C++ that there are |C|-1 values rather that |C|
   #there a no other source on NA's in ranks.of.tags
   if (neglect_diagonal){diag(ranks.of.tags)<-NA}
-  #here, rownames(ranks.of.tags) are the tag names, either by attention parsing of by parameter
-  #here, colnames(ranks.of.tags) are the collection names, either by attention parsing of by parameter
-  #we write something not to have them empty whatever
-
-  #we start to count somethig here
-  #the denominator is the range of ranks, so it is 1 larger than the number of possible friends
-  ranks.of.tags<-(ranks.of.tags-.5)/(dims[1]-as.integer(neglect_diagonal))
-  #there are |C|-1 maximal (default) number friends for general case
-  #default number of friends; if we neglect diagonal, it decreases by 1
-  default.friends.number <- dims[2]-1-as.integer(neglect_diagonal)
-  if(friends.number<=0 || friends.number > default.friends.number){
-    friends.number <- default.friends.number
-  } 
-  #the result "unlistres" contains collection names in even positions and p-values in odd
-  unlistres<-
-    unlist(t(apply(ranks.of.tags,1,rank_diff_and_p_for_the_best_n,max_num_friends=friends.number)))
-
-  res$collections<-matrix(
-    colnames(ranks.of.tags)[
-      unlistres[seq(1,length(unlistres),2)]
-    ],
-    ncol = friends.number+1, nrow=dims[1], byrow = TRUE
-  )
-  #we show one more collection than the friends.number is to know what we separate from
-  res$pvals<-matrix(
-    unlistres[seq(2,length(unlistres),2)],
-    ncol = friends.number+1, nrow=dims[1],byrow = TRUE
-  )[,1:friends.number]
-  rownames(res$collections)<-rownames(ranks.of.tags)
-  rownames(res$pvals)<-rownames(ranks.of.tags)
-  res
-}
+  
