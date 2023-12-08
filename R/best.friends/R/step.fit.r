958c2d680e1260708c65edf576bc37401c376b00
Sasha Favorov <favorov@sensi.org>
2023-12-08 00:27:01 -0500

    preparin description

--- R/best.friends/R/step.fit.r
+++ R/best.friends/R/step.fit.r
@@ -1,3 +1,4 @@
-#'
-#'
-#' maximise likelihood
+#' fit possible bi-uniform step models for a set of ranks
+#' @param ranks vector of ranks of a tag in different collections
+#' @returns the weight on the left of the step and the likelihood of the ranks for each step position in \eqn{1 .. max(ranks)}
+#'   
