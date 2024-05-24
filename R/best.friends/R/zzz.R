43bce8a6623ed5101311aeb1b120f40edd2d64c2
Alexander Favorov <favorov@sensi.org>
2024-05-24 16:20:43 -0400

    Replaced \u00e1 <- á to match new requirements

--- R/best.friends/R/zzz.R
+++ R/best.friends/R/zzz.R
@@ -1,4 +1,5 @@
 .onAttach <- function(libname, pkgname) {
        version <- packageDescription("best.friends", fields="Version")
-       packageStartupMessage(paste("Welcome to best.friends, version", version,"(A Primvera está chegando, e tu?) and stay tuned!"))
+       packageStartupMessage(paste("Welcome to best.friends, version", version,"(A Primvera est\u00e1 chegando, e tu?) and stay tuned!"))
 }
+# \u00e1 is á
