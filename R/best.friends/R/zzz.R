2862d07e40dca18fcc9d9130bf0f0ac3cff62707
Alexander Favorov <favorov@sensi.org>
2024-05-30 19:39:38 -0400

    Unicode codes instad of chearacters, devtools::check() likes it

--- R/best.friends/R/zzz.R
+++ R/best.friends/R/zzz.R
@@ -1,8 +1,9 @@
 .onAttach <- function(libname, pkgname) {
     version <- packageDescription("best.friends", fields="Version")
     packageStartupMessage(paste("Welcome to best.friends, version", version,
-        "(A verão está chegando, e tu?) and stay tuned!")
+        "(A ver\u00E3o est\u00E1 chegando, e tu?) and stay tuned!")
     )
 }
 # \u00E1 is á
 # \u00E3 is ã
+# for detools::check
