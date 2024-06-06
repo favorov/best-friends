2705ed5f50e1fee50b7cfbc46104bedd6beceba2
Alexander Favorov <favorov@sensi.org>
2024-06-06 17:06:35 -0400

    Added collections.order test

--- R/best.friends/tests/testthat/test-step.fit.ln.likelihoods.r
+++ R/best.friends/tests/testthat/test-step.fit.ln.likelihoods.r
@@ -5,10 +5,7 @@ test_that("step.ln.likelihoods returns expected values for known input", {
   expect_equal(length(result$collectons.order), length(ranks))
   expect_equal(length(result$ln.likelihoods), tags.no)
   expect_equal(length(result$k1.by.l1), tags.no)
-  #test for min of result$k1.by.l1
-  #test for max of result$k1.by.l1
-  
-  
+  expect_equal(result$collectons.order, c(1, 2, 3, 4, 5))
 })
 
 test_that("returns error when a rank is higher than tag.no", {
