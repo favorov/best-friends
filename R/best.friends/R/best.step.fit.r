6d625084de99983be7b11a5f3c41684650c62174
Alexander Favorov <favorov@sensi.org>
2024-06-06 16:50:25 -0400

    The first verio of the new best.step.fit function

--- R/best.friends/R/best.step.fit.r
+++ R/best.friends/R/best.step.fit.r
@@ -5,74 +5,37 @@
 #' 
 #' See [best.friends] documentation for details.
 #'
-#' @param ranks vector of ranks of a tag in different collections
-#' @param tags.no number of tags, i.e. maximal rank 
-#' @return a list of three values: \cr
-#' \code{ln.likelihoods} contains the ln of the likelihood of the ranks for each split (step) rank value into (this or less) and (greater than this) for \eqn{1 .. tags.no-1} the last (\eqn{tag.no)} element is for is for uniform, non-step case;\cr
-#' \code{k1.by.l1} contains k_1 (number of values on the left of the step) for each split;\cr
-#' \code{col.order} is the order of ranks in, collection-by-collection\cr
+#' @inheritParams step.fit.ln.likelihoods
+#' @return a list of four values: \cr
+#' \code{step.models} is return from [step.fit.ln.likelihoods] call the function start with
 #' \code{best.step.rank} is the rank value that makes the best step\cr
-#' \code{col.on.left} is the vector of the collections on the left of the best step (including the step value)\cr
+#' \code{col.on.left} is the vector of the collections on the left of the best step 
+#' (including the step value)\cr
 #' \code{col.on.right} is vector of those on the right
+#' \code{population.on.left} is how many ranks are on left of split; they are friends! 
 #' @examples
 #' example(tag.int.ranks)
 #' steps<-step.ln.likelihoods(TF.ranks[42,],genes.no)
 #' @export
 best.step.fit<-function(ranks,tags.no){
   step.models <- step.fit.ln.likelihoods(ranks,tags.no)
-  col.order<-order(ranks)
-  ranks<-ranks[col.order]
-  ln.likelihoods<-rep(0,tags.no)
-  k1.by.l1<-rep(0,tags.no)
-  k<-length(ranks)
-  k1<-0
-  #l1==tags_no is "no step"
-  for (l1 in seq_len(tags.no-1)){ #1:(tags.no-1)
-    while (k1<k && ranks[k1+1]<=l1)
-    {
-      k1<-k1+1      
-    }#l1 has hit next rank value
-    k1.by.l1[l1]<-k1
-    p1<-k1/k
-    if(p1>0){
-      ln.likelihoods[l1]<-
-        ln.likelihoods[l1]+k1*log(p1/l1)
-    }
-    if(p1<1){
-      ln.likelihoods[l1]<-
-        ln.likelihoods[l1]+(k-k1)*log((1-p1)/(tags.no-l1))
-    }
-  }
-  ln.likelihoods[tags.no]<-k*log(1/k)
-  k1.by.l1[tags.no]<-k
+ 
+  possible.best.steps<-seq_len(tags.no-1)
+  k1.by.l1<-step.models$k1.by.l1[possible.best.steps]
+  possible.best.steps<-possible.best.steps[k1.by.l1>0 & k1.by.l1<length(ranks)]
+  #we assess only the steps that have nonzero left and right sets
   
-  best.step.rank<-which.max(ln.likelihoods[seq_len(tags.no-1)]) 
-  #1:tags.no-1
-  
-  #maybe it is an alien plug, maybe not,
-  #still we need it now
-  if (k1.by.l1[best.step.rank]==k && 
-      best.step.rank>1 &&
-      k1.by.l1[best.step.rank-1]>0) 
-  {
-    best.step.rank<-best.step.rank-1
-  } 
+  best.step.rank<-which.max(step.models$ln.likelihoods[possible.best.steps]) 
   
   population.on.left<-k1.by.l1[best.step.rank]
-  #alternative plug
-  #if(population.on.left==tags.no){
-  #  population.on.left<-population.on.left-1
-  #}
-  
-  col.on.left<-col.order[seq_len(population.on.left)] 
+
+  col.on.left<-step.models$col.order[seq_len(population.on.left)] 
   #1:population.on.left
-  col.on.right<-col.order[seq(population.on.left+1,k)] 
+  col.on.right<-step.models$col.order[seq(population.on.left+1,k)] 
   #(population.on.left+1):k
   
   
-  list(ln.likelihoods=ln.likelihoods,
-       k1.by.l1=k1.by.l1,
-       col.order=col.order,
+  list(step.models=step.models,
        best.step.rank=best.step.rank,
        col.on.left=col.on.left,
        col.on.right=col.on.right,
