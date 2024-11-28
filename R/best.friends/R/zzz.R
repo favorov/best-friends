9d1f8de739a8858f59dc8918daf3555654dbde5f
Sasha Favorov <favorov@sensi.org>
2024-11-28 14:26:45 -0500

    Winter again

--- R/best.friends/R/zzz.R
+++ R/best.friends/R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
     version <- packageDescription("best.friends", fields="Version")
     packageStartupMessage(paste("Welcome to best.friends, version", version,
-        "(A ver\u00E3o chegou, e tu?) and stay tuned!")
+        "(A inverno chegou, e tu?) and stay tuned!")
     )
 }
 # \u00E1 is á
