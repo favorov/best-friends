9f8d1bb51cce336bbc336dae14afa9c6a0de596b
Sasha Favorov <favorov@sensi.org>
2025-02-18 23:29:10 +0300

    Prior parameter now is consistent with formulas. Variable names improves as in best.fit The first draft of the likelihood-based choice

--- R/best.friends/R/best.step.fit.bic.r
+++ R/best.friends/R/best.step.fit.bic.r
@@ -8,7 +8,7 @@
 #' See [best.friends] documentation for details.
 #'
 #' @inheritParams step.fit.ln.likelihoods
-#' @param non.step.prior The prior for a tag is not important enough to have no friendly collections.
+#' @param prior.to.have.friends The prior for a tag is important enough to have friendly collections.
 #' @return a list of four values: \cr
 #' \code{step.models} is return from [step.fit.ln.likelihoods] call the function start with
 #' \code{best.step.rank} is the rank value that makes the best step;
@@ -19,33 +19,53 @@
 #' \code{population.on.left} is how many ranks are on left of split; they are friends! \cr
 #' @examples
 #' example(tag.int.ranks)
-#' steps<-step.fit.ln.likelihoods(TF.ranks[42,],genes.no)
+#' steps<-best.step.fit.bic(TF.ranks[42,],genes.no,0.5)
 #' @export
-best.step.fit.bic<-function(ranks, tags.no, non.step.prior=0){
+best.step.fit.bic<-function(ranks, tags.no, prior.to.have.friends){
   step.models <- step.fit.ln.likelihoods(ranks,tags.no)
  
-  possible.best.steps<-seq_len(tags.no-1)
-  k1.by.l1<-step.models$k1.by.l1[possible.best.steps]
-  possible.best.steps<-possible.best.steps[k1.by.l1>0 & k1.by.l1<length(ranks)]
+  possible.step.ranks<-seq_len(tags.no-1)
+  k1.by.l1<-step.models$k1.by.l1[possible.step.ranks]
+  possible.step.ranks<-possible.step.ranks[k1.by.l1>0 & k1.by.l1<length(ranks)]
   #we assess only the steps that have nonzero left and right sets
   
-  #best.step.index<-which.max(step.models$ln.likelihoods[possible.best.steps])
+  #best.step.index<-which.max(step.models$ln.likelihoods[possible.step.ranks])
   #almost; we want the last value, so:
-  possible.likelihoods<-step.models$ln.likelihoods[possible.best.steps]
-  ml<-max(possible.likelihoods)
-  best.step.index<-max(which(possible.likelihoods==ml))
-
-  best.step.rank<-possible.best.steps[best.step.index]
+  possible.ln.likelihoods<-step.models$ln.likelihoods[possible.step.ranks]
+  max.ln.l<-max(possible.ln.likelihoods)
+  #here, we compare the best ln posterior 
+  #with the uniform model posterior,
+  #and here we use the prior.to.have.friends
+  if (max.ln.l+
+        ln(prior.to.have.friends) >= 
+      step.models$ln.likelihoods[tags.no]+
+        ln(1-prior.to.have.friends)) {
+    #if we are here, the step model won
+    best.step.index<-max(which(possible.ln.likelihoods==max.ln.l))
+    #the index in possible.step_ranks (and possible.ln.likelihoods);
+    #we need the rank itself
+    best.step.rank<-possible.step.ranks[best.step.index]
   
-  population.on.left<-k1.by.l1[best.step.rank]
+    population.on.left<-k1.by.l1[best.step.rank]
 
-  collections.on.left<-
+    collections.on.left<-
     step.models$collectons.order[seq_len(population.on.left)] 
-  #1:population.on.left
-  collections.on.right<-
-    step.models$collectons.order[seq(population.on.left+1,length(ranks))] 
-  #(population.on.left+1):k
-  
+    #1:population.on.left
+    collections.on.right<-
+      step.models$collectons.order[seq(population.on.left+1,length(ranks))] 
+    #(population.on.left+1):k
+  } else {
+    #if we are here, the uniform won, no friends
+    
+    best.step.rank<-tags.no
+    
+    population.on.left<-length(ranks) #all
+    
+    collections.on.left<-
+      step.models$collectons.order
+    #all
+    collections.on.right<-c() #empty
+   }
   
   list(step.models=step.models,
        best.step.rank=best.step.rank,
