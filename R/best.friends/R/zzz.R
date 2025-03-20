c6de15e4dc641f45cfad91610eab9a761efeca1b
Sasha Favorov <favorov@sensi.org>
2025-03-20 13:26:44 -0400

    lowercase c with cedilla in the version name

--- R/best.friends/R/zzz.R
+++ R/best.friends/R/zzz.R
@@ -1,9 +1,11 @@
 .onAttach <- function(libname, pkgname) {
     version <- packageDescription("best.friends", fields="Version")
     packageStartupMessage(paste("Welcome to best.friends, version", version,
-        "(Lebre de marco) and stay tuned!")
+        "(Lebre de mar\u00E7o) and stay tuned!")
     )
 }
 # \u00E1 is á
 # \u00E3 is ã
+# \u00E7 is ç
+
 # for detools::check
