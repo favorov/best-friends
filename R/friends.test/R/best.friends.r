eef019972e8b41401b616e5ec58a08bc2715b3f6
Sasha Favorov <favorov@sensi.org>
2025-05-08 11:37:42 -0400

    best.friends -> friends.test best.no -> max.friends.no attention->A changed the explanation in help

--- R/friends.test/R/best.friends.r
+++ R/friends.test/R/best.friends.r
@@ -1,13 +1,20 @@
 #'
-#' best.friends
+#' friends.test
 #' 
-#' Find Tags that are best friends to Collections
+#' We have two sets:T (rows) and C (columns) and 
+#' A real matrix A(t,c) that describes the strength of association 
+#' between each t and each c; t is an element of T and c is an element of C. 
+#' For each t we want to identify whether it is significantly more 
+#' relevant for some c's than for the remaining c's.
+#' If it does, those c for which the t is relevant, 
+#' are the t's friend. And, the t is the c's marker.
 #' 
-#' @param attention original attention matrix
+#' @param A original association matrix
 #' @param threshold The adjusted p-value threshold for KS test for 
 #' non-uniformity of ranks.
-#' @param p.adjust.method Multiple testing correction method, see \link[stats]{p.adjust}.
-#' @param best.no The maximal number of friends for a tag, the default is \code{1}, 
+#' @param p.adjust.method Multiple testing correction method, 
+#' see \link[stats]{p.adjust}.
+#' @param max.friends.no The maximal number of friends for a tag, the default is \code{'all'}, that is an alias for #of collections 
 #' i.e. we look for the best friends only. The string "all" means "all friends", 
 #' i.e. the maximal number of friends in the number of collections
 #' The value $n$ means that we filter out a tag from the results is it has more 
@@ -16,43 +23,43 @@
 #' friends.
 #' @importFrom stats p.adjust
 #' @examples 
-#' attention <- matrix(c(10,6,7,8,9,
+#' A <- matrix(c(10,6,7,8,9,
 #'                 9,10,6,7,8,
 #'                 8,9,10,6,7,
 #'                 7,8,9,10,6,
 #'                 6,7,8,9,10,
 #'                 20,0,0,0,0), 
 #'                 nrow=6, ncol=5, byrow=TRUE)
-#' attention
-#' best.friends(attention, threshold = .05)
-#' best.friends(attention, threshold = .0001)
+#' A
+#' friends.test(A, threshold = .05)
+#' friends.test(A, threshold = .0001)
 #' 
 #' @export
 #' 
-best.friends <- function(attention=NULL, threshold = 0.05, 
-                         p.adjust.method = "BH", best.no = 1) {
+friends.test <- function(A=NULL, threshold = 0.05, 
+                         p.adjust.method = "BH", max.friends.no = 'all') {
   #parameter checks
-  if (is.na(best.no) || best.no == "all" ||
-      best.no == "al" || best.no == "a" ||
-      is.null(best.no) || !as.logical(best.no)){
-    best.no <- ncol(attention)
+  if (is.na(max.friends.no) || max.friends.no == "all" ||
+      max.friends.no == "al" || max.friends.no == "a" ||
+      is.null(max.friends.no) || !as.logical(max.friends.no)){
+    max.friends.no <- ncol(A)
   }
-  if (best.no < 1 || best.no > ncol(attention)) {
-    stop("best.no must be between 1 and the number of collections.")
+  if (max.friends.no < 1 || max.friends.no > ncol(A)) {
+    stop("max.friends.no must be between 1 and the number of collections.")
   }
   if(threshold < 0 || threshold > 1) {
     stop("threshold must be between 0 and 1.")
   }
-  #add names to attention matrix rows if necessary
-  if(is.null(dimnames(attention)[[1]])) {
-    rownames(attention) <- seq(nrow(attention))
+  #add names to A matrix rows if necessary
+  if(is.null(dimnames(A)[[1]])) {
+    rownames(A) <- seq(nrow(A))
   }
-  #add names to attention matrix cols if necessary
-  if(is.null(dimnames(attention)[[2]])) {
-    colnames(attention) <- seq(ncol(attention))
+  #add names to A matrix cols if necessary
+  if(is.null(dimnames(A)[[2]])) {
+    colnames(A) <- seq(ncol(A))
   }
   #find tags with non-uniform ranks
-  all_ranks <- tag.int.ranks(attention)
+  all_ranks <- tag.int.ranks(A)
   adj_nunif_pval <- p.adjust(
       apply(all_ranks, 1, unif.ks.test),
       method = p.adjust.method)
@@ -66,18 +73,18 @@ best.friends <- function(attention=NULL, threshold = 0.05,
 
 
   #find friends that make tag ranks non-uniform
-  tags.no <- dim(attention)[1]
+  tags.no <- dim(A)[1]
 
   all_friends <- apply(marker_ranks, 1,
                        function(x) best.step.fit(x, tags.no = tags.no))
   #we filter to match
-  #best.no parameter here,
+  #max.friends.no parameter here,
   #best friends are cases where a tag is a marker in 
-  #no more than best.no collections
+  #no more than max.friends.no collections
   #vapply is recommended by BioCheck as safer than sapply
 
   best_friends <- all_friends[vapply(all_friends, function(x) {
-    x$population.on.left <= best.no
+    x$population.on.left <= max.friends.no
   },logical(1))]
   
   if(!length(best_friends)){
