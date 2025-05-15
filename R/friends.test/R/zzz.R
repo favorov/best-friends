a6a557a404b02805195a37b787667a65af7816d7
Alexander Favorov <favorov@sensi.org>
2025-05-15 01:14:29 -0400

    Salutation

--- R/friends.test/R/zzz.R
+++ R/friends.test/R/zzz.R
@@ -1,11 +1,12 @@
 .onAttach <- function(libname, pkgname) {
     version <- packageDescription("friends.test", fields="Version")
     packageStartupMessage(paste("Welcome to friends.test, version", version,
-        "(Lebre de maio) and stay tuned!")
+        "(As f\u00E9rias de maio) and stay tuned!")
     )
 }
 # \u00E1 is á
 # \u00E3 is ã
 # \u00E7 is ç
+# \u00E9 is é
 
 # for detools::check
